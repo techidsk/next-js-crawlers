1842408a6c02fe74781094c27e93a74ad6eb8cdc
techidsk <ecpknymt@gmail.com>
2022-01-19 17:22:37 +0800

    新增日期筛选

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/global.css'
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
 import { MediaProvider } from '../lib/medias'
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function App({ Component, pageProps }) {
   return <MediaProvider>
--- pages/media/[id].js
+++ pages/media/[id].js
@@ -8,6 +8,9 @@ import { Table, Pagination, Divider, Button, Input, Spacer, Collapse, Grid } fro
 import axios from 'axios'
 import _ from 'lodash'
 import { Column } from '@antv/g2plot';
+import DatePicker, { registerLocale } from "react-datepicker";
+import zh from 'date-fns/locale/zh-CN';
+registerLocale('zh', zh)
 
 const dayjs = require('dayjs')
 
@@ -17,6 +20,11 @@ export default function Post({ postData }) {
     const [title, setTitle] = useState('')
     const [page, setPage] = useState(1)
     const [pagination, setPagination] = useState({ size: 15, num: 1, count: 1 })
+
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [num, setNum] = useState(0);
+
     const router = useRouter()
     const { id } = router.query
 
@@ -57,6 +65,21 @@ export default function Post({ postData }) {
         fetchData()
     }, [pagination.num, id])
 
+    const searchCount = async () => {
+        await axios({
+            method: 'post',
+            url: 'http://site.yunzitui.com:7788/api/order/count',
+            // url: 'http://localhost:7788/api/order/count',
+            data: qs.stringify({
+                startTime: dayjs(startDate).format('YYYY-MM-DD hh:mm:ss'),
+                endTime: dayjs(endDate).format('YYYY-MM-DD hh:mm:ss'),
+                media_id: _.split(id, '-')[0]
+            })
+        }).then(function (response) {
+            setNum(response.data)
+        })
+    }
+
     let pageData = _.filter(postData, e => e.name === id)[0]
 
     return <Layout>
@@ -80,6 +103,21 @@ export default function Post({ postData }) {
             </Grid>
         </Grid.Container >
         <Spacer y={1} />
+        <Grid.Container gap={1}>
+            <Grid xs={6}>
+                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} locale="zh" />
+            </Grid>
+            <Grid xs={6}>
+                <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} locale="zh" />
+            </Grid>
+            <Grid xs={6}>
+                <Button onClick={() => searchCount()} scale={0.8} type="success">搜索</Button>
+            </Grid>
+            <Grid xs={6}>
+                <div>数量：{num}</div>
+            </Grid>
+        </Grid.Container >
+        <Spacer y={1} />
         <Collapse title="发布数量表">
             <div id="container"></div>
         </Collapse>
@@ -93,10 +131,10 @@ export default function Post({ postData }) {
         <Divider />
         <Pagination count={pagination.count} initialPage={1} page={pagination.num} limit={pagination.size} onChange={e => setPagination({ ...pagination, num: e })} />
         <div>
-            <span>跳转至</span>
+            <span>跳转至 </span>
             <Input placeholder="跳转至" value={page} onChange={e => setPage(e.target.value)} />
-            <span>页</span>
-            <Button onClick={() => setPagination({ ...pagination, num: page })} scale={0.8} type="success">确认</Button>
+            <span> 页     </span>
+            <Button onClick={() => setPagination({ ...pagination, num: page })} scale={0.8} auto type="success">确认</Button>
         </div>
     </Layout>
 }
